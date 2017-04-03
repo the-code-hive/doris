669609efdfd5c97450575ed5f64f14c90d47b6d4
the-code-hive <elizabeth.matthews.lai@gmail.com>
2017-04-03 14:32:13 +0000

    inital commit

---
+++ Doris1.py
@@ -0,0 +1,35 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+G = [255, 0, 0] #rgb green
+P = [255, 255, 255]#rgb purple
+B = #rgb blue
+
+Fish1 = [
+O, O, O, X, X, O, O, O,
+O, O, X, O, O, X, O, O,
+O, O, O, O, O, X, O, O,
+O, O, O, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, X, O, O, O, O
+]
+
+Fish2 = [
+O, O, O, X, X, O, O, O,
+O, O, X, O, O, X, O, O,
+O, O, O, O, O, X, O, O,
+O, O, O, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, X, O, O, O, O
+]
+
+sense.set_pixels(question_mark)
+sleep(.5)
+sense.set_pixels(question_mark)
+sleep(.5)
